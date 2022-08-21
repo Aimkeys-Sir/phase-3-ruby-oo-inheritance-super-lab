2745be6af90f0830136fcc4d459771c0708ea65b
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-21 21:10:58 +0300

    commit tree 46618fd39fc813555bf9b42b6f8388931a0454ec

---
+++ lib/chatty_student.rb
@@ -0,0 +1,18 @@
+require_relative 'student'
+
+class ChattyStudent <Student
+    def hello
+        super
+       puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+        #  .scan(/\s+[^.!?]*[.!?]/)
+            # /^\\s+[A-Za-z,;'\"\\s]+[.?!]$"/)
+    end
+
+    def raise_hand
+        10.times{super}
+    end
+end
+
+me=ChattyStudent.new
+
+me.hello
